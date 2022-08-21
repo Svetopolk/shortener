a9ea166fda3b42c4df35e6f8cccfcadbad72c70f
asmyasnikov <79263256394@ya.ru>
2022-08-21 12:54:24 +0300

    fix tests

--- internal/app/db/db_test.go
+++ internal/app/db/db_test.go
@@ -5,25 +5,30 @@ import (
 
 	"github.com/Svetopolk/shortener/internal/app/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPingWrongPort(t *testing.T) {
-	db := NewDB("postgres://shortener:pass@localhost:5433/shortener")
-	err := db.Ping()
-	assert.Contains(t, err.Error(), "failed to connect")
+	db, err := NewDB("postgres://shortener:pass@localhost:5433/shortener")
+	require.NoError(t, err)
+	err = db.Ping()
+	require.NoError(t, err)
 }
 
 //before run this
 //docker run --name postgresql -e POSTGRES_USER=shortener -e POSTGRES_PASSWORD=pass -p 5432:5432 -d postgres
 
 func _TestPingOk(t *testing.T) {
-	db := NewDB("postgres://shortener:pass@localhost:5432/shortener")
-	err := db.Ping()
-	assert.Equal(t, nil, err)
+	db, err := NewDB("postgres://shortener:pass@localhost:5432/shortener")
+	require.NoError(t, err)
+	err = db.Ping()
+	require.NoError(t, err)
 }
 
 func _TestSaveGet(t *testing.T) {
-	db := NewDB("postgres://shortener:pass@localhost:5432/shortener")
+	db, err := NewDB("postgres://shortener:pass@localhost:5432/shortener")
+	require.NoError(t, err)
+
 	hash := util.RandomString(5)
 
 	db.Save(hash, "someUrl")
@@ -33,7 +38,9 @@ func _TestSaveGet(t *testing.T) {
 }
 
 func _TestGetEmpty(t *testing.T) {
-	db := NewDB("postgres://shortener:pass@localhost:5432/shortener")
+	db, err := NewDB("postgres://shortener:pass@localhost:5432/shortener")
+	require.NoError(t, err)
+
 	hash := util.RandomString(5)
 
 	urlFromDB := db.Get(hash)
