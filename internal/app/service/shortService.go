c4aeb21b919a386babccd6405f92e4e7f139d572
Stanislav E Mikhaylov <SEMikhaylov@alfabank.ru>
2022-08-22 17:34:42 +0300

    sprint3 increment11 fix recursive call 2

--- internal/app/service/shortService.go
+++ internal/app/service/shortService.go
@@ -63,5 +63,5 @@ func (s *ShortService) checkOrChange(hash string) string {
 
 	newHash := hash + util.RandomString(1)
 	log.Printf("hash %s already exist, generate a new one %s", hash, newHash)
-	return s.checkOrChange(hash)
+	return s.checkOrChange(newHash)
 }
